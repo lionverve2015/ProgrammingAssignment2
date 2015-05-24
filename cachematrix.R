813fdd03b6eff5c0a4c45245d02b8e4007d08f7a
lionverve2015 <lcasanova30@hotmail.com>
2015-05-25 04:18:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Problem Assigngment 2
 
-## Write a short comment describing this function
+# createCacheMatrix makes a special matrix object, and then 
+# docacheSolve calculates the inverse of the matrix.
+# If the matrix inverse has been calculated, it will simply 
+# find it in the cache and return it, and not re-calculate it.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+createCacheMatrix <- function(x = matrix()) {
+  invrs_x <- NULL
+  set <- function(y) {
+    x <<- y
+    invrs_x <<- NULL
+  }
+  get <- function() x
+  setinverse<- function(inverse) invrs_x <<-inverse
+  getinverse <- function() invrs_x
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+# The function docacheSolve returns the inverse of a matrix A created with
+# the above function.
+# If the cached inverse is available, docacheSolve retrieves it,but if not, 
+# it computes, caches, and returns it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+docacheSolve <- function(x, ...) {
+  # Return a matrix that is the inverse of 'x'
+  invrs_x <- x$getinverse()
+  if (!is.null(invrs_x)) {
+    message("getting cached inverse matrix")
+    return(invrs_x)
+  } else {
+    invrs_x <- solve(x$get())
+    x$setinverse(invrs_x)
+    return(invrs_x)
+  }
 }
+
+math1 <- matrix(data = c(4,2,7,6), nrow = 2, ncol = 2)
+math2 <- createCacheMatrix(math1)
+docacheSolve(math2)
